15a8606b18d104416f93d3ac812ad2865dd01b5c
Steven Kha <stevenvkha@gmail.com>
2018-10-27 13:04:10 -0700

    animate pacman and ghosts

--- ghost.py
+++ ghost.py
@@ -3,7 +3,7 @@ from settings import Settings
 from imagerect import ImageRect
 # import random
 import time
-
+from timer import Timer
 class BlueGhost():
     P1_SIZE = 33
     def __init__(self, screen, mazefile, p1):
@@ -17,8 +17,9 @@ class BlueGhost():
         self.p1s = []
         size = BlueGhost.P1_SIZE
 
-        self.p1 = ImageRect(screen, p1, size, size)
 
+        self.curFrame = Timer(self.blueGhosts)
+        self.p1 = ImageRect(screen, p1, size, size)
         r = self.p1.rect
         w, h = r.width, r.height
 
@@ -33,13 +34,19 @@ class BlueGhost():
             self.x = float(rect.x)
             self.y = float(rect.y)
     def update(self, maze):
+
         # add pacSettings to reverse movement
+        # print("current frame: " + str( self.curFrame.imagerect() ))
         time = pygame.time.get_ticks()
         # randx = random.randint(1, 10)
         # even = False
         # odd = False
 
         if not self.collide_x:
+            if self.x_direction < 0:
+                self.p1.image = pygame.image.load(self.blueGhosts[3])
+            else:
+                self.p1.image = pygame.image.load(self.blueGhosts[0])
             self.x += 1 * self.x_direction
             for rect in self.p1s:
                 rect.x = self.x
@@ -48,7 +55,7 @@ class BlueGhost():
                         self.collide_x = True
                         if self.x_direction > 0:
                             # print("I ran into the wall at the right")
-                            self.image = pygame.image.load(self.blueGhosts[3])
+
 
                             # need to stop Pacman from touching the walls
                             # then get Pacman not colliding with anything
@@ -56,7 +63,7 @@ class BlueGhost():
                             self.x = self.x -1
                             rect.x = self.x
                         elif self.x_direction < 0:
-                            self.image = pygame.image.load(self.blueGhosts[0])
+
                             # print("I ran into the wall at the left")
                             self.x = self.x + 1
                             rect.x = self.x
@@ -71,6 +78,10 @@ class BlueGhost():
 
 
         if self.collide_x:
+            if self.y_direction < 0:
+                self.p1.image = pygame.image.load(self.blueGhosts[1])
+            else:
+                self.p1.image = pygame.image.load(self.blueGhosts[2])
             self.y += 1 * self.y_direction
             for rect in self.p1s:
                 rect.y = self.y
@@ -78,12 +89,12 @@ class BlueGhost():
                     if rect.colliderect(rect2):
 
                         if self.y_direction > 0:
-                            self.image = pygame.image.load(self.blueGhosts[1])
+                            # self.image = pygame.image.load(self.blueGhosts[1])
                             self.y = self.y - 3
                             rect.y = self.y
                             # print("I ran into the wall at the bottom")
                         elif self.y_direction < 0:
-                            self.image = pygame.image.load(self.blueGhosts[2])
+                            # self.image = pygame.image.load(self.blueGhosts[2])
                             self.y = self.y + 3
                             rect.y = self.y
                             # print("I ran into the wall at the top")
@@ -109,8 +120,8 @@ class RedGhost():
         with open(self.filename, "r") as f:
             self.rows = f.readlines()
 
-        self.redGhosts = ['images/BlueGhost0.png', 'images/BlueGhost1.png',
-                           'images/BlueGhost2.png', 'images/BlueGhost3.png']
+        self.redGhosts = ['images/redGhost0.png', 'images/redGhost1.png',
+                           'images/redGhost2.png', 'images/redGhost3.png']
         self.reds = []
         size = BlueGhost.P1_SIZE
 
@@ -138,6 +149,10 @@ class RedGhost():
         # odd = False
 
         if not self.collide_x:
+            if self.x_direction < 0:
+                self.red.image = pygame.image.load(self.redGhosts[2])
+            else:
+                self.red.image = pygame.image.load(self.redGhosts[3])
             self.x += 1 * self.x_direction
             for rect in self.reds:
                 rect.x = self.x
@@ -169,6 +184,10 @@ class RedGhost():
 
 
         if self.collide_x:
+            if self.y_direction < 0:
+                self.red.image = pygame.image.load(self.redGhosts[0])
+            else:
+                self.red.image = pygame.image.load(self.redGhosts[1])
             self.y += 1 * self.y_direction
             for rect in self.reds:
                 rect.y = self.y
@@ -196,7 +215,7 @@ class RedGhost():
     def blitme(self):
         for rect in self.reds:
             self.screen.blit(self.red.image, rect)
-                        
+
 class YellowGhost():
     P1_SIZE = 33
     def __init__(self, screen, mazefile, yellow):
@@ -205,8 +224,8 @@ class YellowGhost():
         with open(self.filename, "r") as f:
             self.rows = f.readlines()
 
-        self.yellowGhosts = ['images/BlueGhost0.png', 'images/BlueGhost1.png',
-                           'images/BlueGhost2.png', 'images/BlueGhost3.png']
+        self.yellowGhosts = ['images/yellowGhost0.png', 'images/yellowGhost1.png',
+                           'images/yellowGhost2.png', 'images/yellowGhost3.png']
         self.yellows = []
         size = BlueGhost.P1_SIZE
 
@@ -234,6 +253,10 @@ class YellowGhost():
         # odd = False
 
         if not self.collide_x:
+            if self.x_direction < 0:
+                self.yellow.image = pygame.image.load(self.yellowGhosts[1])
+            else:
+                self.yellow.image = pygame.image.load(self.yellowGhosts[2])
             self.x += 1 * self.x_direction
             for rect in self.yellows:
                 rect.x = self.x
@@ -265,6 +288,10 @@ class YellowGhost():
 
 
         if self.collide_x:
+            if self.y_direction < 0:
+                self.yellow.image = pygame.image.load(self.yellowGhosts[3])
+            else:
+                self.yellow.image = pygame.image.load(self.yellowGhosts[0])
             self.y += 1 * self.y_direction
             for rect in self.yellows:
                 rect.y = self.y
@@ -292,7 +319,7 @@ class YellowGhost():
     def blitme(self):
         for rect in self.yellows:
             self.screen.blit(self.yellow.image, rect)
-            
+
 class PinkGhost():
     P1_SIZE = 33
     def __init__(self, screen, mazefile, pink):
@@ -301,8 +328,8 @@ class PinkGhost():
         with open(self.filename, "r") as f:
             self.rows = f.readlines()
 
-        self.pinkGhosts = ['images/BlueGhost0.png', 'images/BlueGhost1.png',
-                           'images/BlueGhost2.png', 'images/BlueGhost3.png']
+        self.pinkGhosts = ['images/pinkGhost0.png', 'images/pinkGhost1.png',
+                           'images/pinkGhost2.png', 'images/pinkGhost3.png']
         self.pinks = []
         size = BlueGhost.P1_SIZE
 
@@ -330,6 +357,11 @@ class PinkGhost():
         # odd = False
 
         if not self.collide_x:
+            # if self.x_direction < 0:
+            if self.x_direction < 0:
+                self.pink.image = pygame.image.load(self.pinkGhosts[2])
+            else:
+                self.pink.image = pygame.image.load(self.pinkGhosts[3])
             self.x += 1 * self.x_direction
             for rect in self.pinks:
                 rect.x = self.x
@@ -361,6 +393,10 @@ class PinkGhost():
 
 
         if self.collide_x:
+            if self.y_direction < 0:
+                self.pink.image = pygame.image.load(self.pinkGhosts[0])
+            else:
+                self.pink.image = pygame.image.load(self.pinkGhosts[1])
             self.y += 1 * self.y_direction
             for rect in self.pinks:
                 rect.y = self.y
--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 import pygame
 from imagerect import ImageRect
+import time
+from timer import Timer
 class Player():
     P1_SIZE = 35
     def __init__(self, screen, mazefile, p1):
@@ -8,8 +10,11 @@ class Player():
         with open(self.filename, "r") as f:
             self.rows = f.readlines()
 
-        self.blueGhosts = ['images/BlueGhost0.png', 'images/BlueGhost1.png',
-                           'images/BlueGhost2.png', 'images/BlueGhost3.png']
+        self.moveRight = ['images/pacman3.png', 'images/pacman4.png']
+        self.moveLeft = ['images/pacman0.png', 'images/pacman1.png']
+        self.moveUp = ['images/pacman5.png', 'images/pacman6.png']
+        self.moveDown = ['images/pacman7.png', 'images/pacman8.png']
+
         self.players = []
         size = Player.P1_SIZE
 
@@ -38,15 +43,26 @@ class Player():
 
         self.x_direction = 2
         self.y_direction = 2
+        self.animate = 0
 
     def update(self, maze):
         # add pacSettings to reverse movement
-        time = pygame.time.get_ticks()
+        now = pygame.time.get_ticks()
+        moveRightFrame = Timer(self.moveRight)
+
 
         for rect in self.players:
             # if self.y_hit is not self.y:
             #     self.hit_right = False
             if self.moving_right:
+                if now % 15 == 0:
+                    self.p1.image = pygame.image.load(self.moveRight[1])
+                    # self.animate += 1
+                elif now % 32 == 0:
+                    self.p1.image = pygame.image.load(self.moveRight[0])
+                    # self.animate = 0
+                # self.p1.image = pygame.image.load(moveRightFrame.imagerect())
+                # print("right animate frame: " + str(moveRightFrame.imagerect()))
                 self.x += self.x_direction
                 rect.x = self.x
                 for rect2 in maze.bricks:
@@ -58,15 +74,24 @@ class Player():
                         return
 
             if self.moving_down:
+                if now % 15 == 0:
+                    self.p1.image = pygame.image.load(self.moveDown[1])
+                elif now % 32 == 0:
+                    self.p1.image = pygame.image.load(self.moveDown[0])
                 self.y += self.y_direction
                 rect.y = self.y
                 for rect2 in maze.bricks:
                     if rect.colliderect(rect2):
+                    # if pygame.sprite.collide_rect(rect, rect2):
                         self.y = self.y - self.y_direction
                         rect.y = self.y
                         return
 
             if self.moving_left:
+                if now % 15 == 0:
+                    self.p1.image = pygame.image.load(self.moveLeft[1])
+                elif now % 32 == 0:
+                    self.p1.image = pygame.image.load(self.moveLeft[0])
                 self.x -= self.x_direction
                 rect.x = self.x
                 for rect2 in maze.bricks:
@@ -78,6 +103,10 @@ class Player():
                         return
 
             if self.moving_up:
+                if now % 15 == 0:
+                    self.p1.image = pygame.image.load(self.moveUp[1])
+                elif now % 32 == 0:
+                    self.p1.image = pygame.image.load(self.moveUp[0])
                 self.y -= self.y_direction
                 rect.y = self.y
                 for rect2 in maze.bricks:
